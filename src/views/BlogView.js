26297c235ab4db51cdb8c3cff58811c9f2cece4f
JeffreyYu2018 <jeff.yu831@gmail.com>
2020-02-24 18:59:30 -0500

    appfairy: Migrate

--- src/views/BlogView.js
+++ src/views/BlogView.js
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import { createScope, map, transformProxies } from './helpers'
-import PostsListView from './PostsListView'
 
 const scripts = [
   fetch("https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.4.1.min.220afd743d.js").then(body => body.text()),
@@ -11,19 +10,19 @@ const scripts = [
 
 let Controller
 
-class IndexView extends React.Component {
+class BlogView extends React.Component {
   static get Controller() {
     if (Controller) return Controller
 
     try {
-      Controller = require('../controllers/IndexController')
+      Controller = require('../controllers/BlogController')
       Controller = Controller.default || Controller
 
       return Controller
     }
     catch (e) {
       if (e.code == 'MODULE_NOT_FOUND') {
-        Controller = IndexView
+        Controller = BlogView
 
         return Controller
       }
@@ -47,11 +46,16 @@ class IndexView extends React.Component {
   }
 
   render() {
-    const proxies = Controller !== IndexView ? transformProxies(this.props.children) : {
+    const proxies = Controller !== BlogView ? transformProxies(this.props.children) : {
       'brand-nav-link': [],
       'home-nav-link': [],
       'about-nav-link': [],
       'contact-nav-link': [],
+      'post-image': [],
+      'blog-title': [],
+      'post-info': [],
+      'post-body': [],
+      'back-to-home-nav-link': [],
     }
 
     return (
@@ -62,7 +66,7 @@ class IndexView extends React.Component {
           @import url(/css/jeff-appfairy-test.webflow.css);
         ` }} />
         <span className="af-view">
-          <div className="af-class-body">
+          <div className="af-class-body-2">
             <div data-collapse="medium" data-animation="default" data-duration={400} className="af-class-navigation-bar w-nav">
               <div className="w-container">
                 {map(proxies['brand-nav-link'], props => <a href="#" {...{...props, className: `w-nav-brand ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>
@@ -76,7 +80,7 @@ class IndexView extends React.Component {
             </div>
             <div className="af-class-content-wrapper">
               <div className="w-container">
-                <div className="af-class-columns w-row">
+                <div className="w-row">
                   <div className="w-hidden-small w-hidden-tiny w-col w-col-3">
                     <div className="af-class-white-wrapper"><img src="images/LinkedIn-Photo-Square.jpg" srcSet="images/LinkedIn-Photo-Square-p-500.jpeg 500w, images/LinkedIn-Photo-Square-p-800.jpeg 800w, images/LinkedIn-Photo-Square-p-1080.jpeg 1080w, images/LinkedIn-Photo-Square-p-1600.jpeg 1600w, images/LinkedIn-Photo-Square-p-2000.jpeg 2000w, images/LinkedIn-Photo-Square-p-2600.jpeg 2600w, images/LinkedIn-Photo-Square-p-3200.jpeg 3200w, images/LinkedIn-Photo-Square.jpg 3456w" sizes="(max-width: 767px) 100vw, (max-width: 991px) 97.296875px, 126px" alt className="af-class-circle-profile" />
                       <p className="af-class-site-description">Hi! My name is Jeffrey, and I wanted to welcome you to my site! I'm currently a student at Yale University studying computer science. I'm a software developer, tennis enthusiast, and travel blogger, and I'm excited to give you a look into my life. This is my temporary blog that I will be using until I&nbsp;develop my full personal website. Thank you for stopping by!</p>
@@ -85,12 +89,33 @@ class IndexView extends React.Component {
                     </div>
                   </div>
                   <div className="af-class-content-column w-col w-col-9">
-                    <PostsListView.Controller />
-                    <div className="af-class-sidebar-on-mobile">
-                      <div className="af-class-white-wrapper"><img src="images/LinkedIn-Photo-Square.jpg" srcSet="images/LinkedIn-Photo-Square-p-500.jpeg 500w, images/LinkedIn-Photo-Square-p-800.jpeg 800w, images/LinkedIn-Photo-Square-p-1080.jpeg 1080w, images/LinkedIn-Photo-Square-p-1600.jpeg 1600w, images/LinkedIn-Photo-Square-p-2000.jpeg 2000w, images/LinkedIn-Photo-Square-p-2600.jpeg 2600w, images/LinkedIn-Photo-Square-p-3200.jpeg 3200w, images/LinkedIn-Photo-Square.jpg 3456w" sizes="(max-width: 767px) 100vw, (max-width: 991px) 97.296875px, 126px" alt className="af-class-circle-profile" />
-                        <p className="af-class-site-description">Hi! My name is Jeffrey, and I wanted to welcome you to my site! I'm currently a student at Yale University studying computer science. I'm a software developer, tennis enthusiast, and travel blogger, and I'm excited to give you a look into my life. This is my temporary blog that I will be using until I&nbsp;develop my full personal website. Thank you for stopping by!</p>
+                    <div className="af-class-post-wrapper">{map(proxies['post-image'], props => <img src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg" alt {...{...props, className: `af-class-post-image ${props.className || ''}`}}>{props.children}</img>)}
+                      <div className="af-class-posts-list">
+                        {map(proxies['blog-title'], props => <h1 {...props}>{props.children ? props.children : <React.Fragment>Heading</React.Fragment>}</h1>)}
+                        <div className="af-class-details-wrapper">
+                          {map(proxies['post-info'], props => <div {...{...props, className: `af-class-post-info ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>This is some text inside of a div block.</React.Fragment>}</div>)}
+                          <div className="af-class-post-info">|</div><a href="#" className="af-class-post-info af-class-when-link">Text Link</a></div>
                         <div className="af-class-grey-rule" />
-                        <div className="af-class-social-link-group"><a href="https://www.facebook.com/jeffrey.yu.737" className="af-class-social-icon-link w-inline-block"><img src="images/social-03.svg" width={25} alt /></a><a href="https://www.instagram.com/jeffreyyu237/" className="af-class-social-icon-link w-inline-block"><img src="images/social-07.svg" width={25} alt /></a><a href="https://github.com/JeffreyYu2018" className="af-class-social-icon-link w-inline-block"><img src="images/github.svg" width={25} alt /></a><a href="https://www.linkedin.com/in/jeffrey-yu-/" className="af-class-social-icon-link w-inline-block"><img src="images/social-09.svg" width={25} alt /></a></div>
+                        {map(proxies['post-body'], props => <div {...{...props, className: `af-class-body-copy w-richtext ${props.className || ''}`}}>{props.children}</div>)}
+                      </div>
+                    </div>
+                    <div className="af-class-button-wrapper">{map(proxies['back-to-home-nav-link'], props => <a href="#" {...{...props, className: `af-class-button w-button ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>←&nbsp;Back to Home</React.Fragment>}</a>)}</div>
+                    <div className="af-class-sidebar-on-mobile">
+                      <div className="af-class-white-wrapper"><img src="images/photo-1443180236447-432ea00e6ead.jpg" alt className="af-class-circle-profile" />
+                        <p className="af-class-site-description">Denali is a simple responsive blog template. Easily add new posts using the Editor or change layout and design using the Designer.</p>
+                        <div className="af-class-grey-rule" />
+                        <h2 className="af-class-small-heading">Featured Posts:</h2>
+                        <div className="af-class-feature-posts-list w-dyn-list">
+                          <div className="w-dyn-items">
+                            <div className="w-dyn-item" />
+                          </div>
+                          <div className="w-dyn-empty">
+                            <p>No items found.</p>
+                          </div>
+                        </div>
+                        <div className="af-class-grey-rule" />
+                        <div className="af-class-social-link-group"><a href="#" className="af-class-social-icon-link w-inline-block"><img src="https://uploads-ssl.webflow.com/5e4ef3e0507f132c10405130/5e4ef3e01374657704c020dc_social-03.svg" width={25} alt /></a><a href="#" className="af-class-social-icon-link w-inline-block"><img src="https://uploads-ssl.webflow.com/5e4ef3e0507f132c10405130/5e4ef3e01374650b35c02122_social-07.svg" width={25} alt /></a><a href="#" className="af-class-social-icon-link w-inline-block"><img src="https://uploads-ssl.webflow.com/5e4ef3e0507f132c10405130/5e4ef3e01374657296c02161_social-18.svg" width={25} alt /></a><a href="#" className="af-class-social-icon-link w-inline-block"><img src="https://uploads-ssl.webflow.com/5e4ef3e0507f132c10405130/5e4ef3e0137465a6e5c020e9_social-09.svg" width={25} alt /></a></div>
+                        <p className="af-class-built-with-webflow">Built with <a target="_blank" href="https://webflow.com" className="af-class-webflow-link">Webflow</a></p>
                       </div>
                     </div>
                   </div>
@@ -105,6 +130,6 @@ class IndexView extends React.Component {
   }
 }
 
-export default IndexView
+export default BlogView
 
 /* eslint-enable */
