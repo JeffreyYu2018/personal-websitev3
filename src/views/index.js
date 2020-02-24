816e8f3fc615ddfdcc3d569cac95115d859377c0
JeffreyYu2018 <jeff.yu831@gmail.com>
2020-02-24 18:56:52 -0500

    appfairy: Migrate

--- src/views/index.js
+++ src/views/index.js
@@ -3,7 +3,6 @@
 export { default as PostsListView } from './PostsListView'
 export { default as UnauthorizedView } from './UnauthorizedView'
 export { default as AboutView } from './AboutView'
-export { default as BlogView } from './BlogView'
 export { default as ContactView } from './ContactView'
 export { default as DetailCategoriesView } from './DetailCategoriesView'
 export { default as DetailPostsView } from './DetailPostsView'
