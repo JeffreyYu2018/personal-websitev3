2368ec729b148db5e925f65864b45406c9998e65
JeffreyYu2018 <jeff.yu831@gmail.com>
2020-02-24 18:44:52 -0500

    Add ReactTruncateMarkdown for post list controller

--- src/controllers/IndexController.js
+++ src/controllers/IndexController.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import IndexView from '../views/IndexView'
+import { withRouter } from 'react-router-dom'
 
-export default (props) => (
-  <IndexView>
-    <brand-nav-link href="/" />
-    <home-nav-link href="/" />
-    <about-nav-link href="/about" />
-    <contact-nav-link href="/contact" />
-  </IndexView>
-)
+class IndexController extends React.Component {
+  render() {
+    return (
+      <IndexView>
+        <brand-nav-link href="/" />
+        <home-nav-link href="/" />
+        <about-nav-link href="/about" />
+        <contact-nav-link href="/contact" />
+      </IndexView>
+    )
+  }
+}
+
+export default withRouter(IndexController);
