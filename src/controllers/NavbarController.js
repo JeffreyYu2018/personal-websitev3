430b5ea5e5407f7a448f6f0c52b64ff0b7798e95
JeffreyYu2018 <jeff.yu831@gmail.com>
2020-02-25 17:39:11 -0500

    Add sockets for navbar menu button and icon

--- src/controllers/NavbarController.js
+++ src/controllers/NavbarController.js
@@ -3,31 +3,23 @@ import NavbarView from '../views/NavbarView'
 
 const stateToggle = true
 
+// Note: the nav-overlay capability was not transferred over with appfairy, so I had to hardcode it
 class NavbarController extends React.Component {
   render() {
     return (
-      <div>
-        <NavbarView>
-          <nav-menu>
-            <brand-nav-link href="/" />
-            <home-nav-link href="/" />
-            <about-nav-link href="/about" />
-            <contact-nav-link href="/contact" />
-          </nav-menu>
-        </NavbarView>
-        { stateToggle ? (
-          <div class="w-nav-overlay" data-wf-ignore="" style={{height:"1124.23px",display:"block"}}>
-            <nav role="navigation" class="af-class-navigation-menu w-nav-menu" style={{transform:"translateY(0px) translateX(0px)",transition:"transform 400ms ease 0s"}} data-nav-menu-open="">
-              <a href="index.html" class="af-class-navigation-link w-nav-link w--current w--nav-link-open" style={{}}>Home</a>
-              <a href="about.html" class="af-class-navigation-link w-nav-link w--nav-link-open" style={{}}>About</a>
-              <a href="contact.html" class="af-class-navigation-link w-nav-link w--nav-link-open" style={{}}>Contact</a>
-            </nav>
-          </div>
-        ) : (
-          <p>Nothing here.</p>
-        )
-        }
-      </div>
+      <NavbarView>
+        <brand-nav-link href="/" />
+        <home-nav-link href="/" />
+        <about-nav-link href="/about" />
+        <contact-nav-link href="/contact" />
+        <nav-overlay class="w-nav-overlay" data-wf-ignore="" style={{display:"block"}}>
+          <nav role="navigation" class="af-class-navigation-menu w-nav-menu" style={{transform:"translateY(0px) translateX(0px)",transition:"transform 400ms ease 0s"}} data-nav-menu-open="">
+            <a href="/" class="af-class-navigation-link w-nav-link w--current w--nav-link-open">Home</a>
+            <a href="/about" class="af-class-navigation-link w-nav-link w--nav-link-open">About</a>
+            <a href="/contact" class="af-class-navigation-link w-nav-link w--nav-link-open">Contact</a>
+          </nav>
+        </nav-overlay>
+      </NavbarView>
     )
   }
 }
