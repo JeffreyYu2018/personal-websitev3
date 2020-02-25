7e8b610dc6ff6a563fefbf24c019dfa1b34d8a71
JeffreyYu2018 <jeff.yu831@gmail.com>
2020-02-25 17:39:15 -0500

    appfairy: Migrate

--- src/views/NavbarView.js
+++ src/views/NavbarView.js
@@ -47,10 +47,11 @@ class NavbarView extends React.Component {
   render() {
     const proxies = Controller !== NavbarView ? transformProxies(this.props.children) : {
       'brand-nav-link': [],
-      'nav-menu': [],
       'home-nav-link': [],
       'about-nav-link': [],
       'contact-nav-link': [],
+      'navbar-menu-button': [],
+      'navbar-menu-icon': [],
       'nav-overlay': [],
     }
 
@@ -67,10 +68,10 @@ class NavbarView extends React.Component {
               {map(proxies['brand-nav-link'], props => <a href="#" {...{...props, className: `w-nav-brand ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>
                 <div className="af-class-site-name">Jeffrey Yu</div>
               </React.Fragment>}</a>)}
-              {map(proxies['nav-menu'], props => <nav role="navigation" {...{...props, className: `af-class-navigation-menu w-nav-menu ${props.className || ''}`}}>{createScope(props.children, proxies => <React.Fragment>{map(proxies['home-nav-link'], props => <a href="index.html" {...{...props, className: `af-class-navigation-link w-nav-link w--current ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>Home</React.Fragment>}</a>)}{map(proxies['about-nav-link'], props => <a href="about.html" {...{...props, className: `af-class-navigation-link w-nav-link ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>About</React.Fragment>}</a>)}{map(proxies['contact-nav-link'], props => <a href="contact.html" {...{...props, className: `af-class-navigation-link w-nav-link ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>Contact</React.Fragment>}</a>)}</React.Fragment>)}</nav>)}
-              <div className="af-class-menu-button w-nav-button">
-                <div className="w-icon-nav-menu" />
-              </div>
+              <nav role="navigation" className="af-class-navigation-menu w-nav-menu">{map(proxies['home-nav-link'], props => <a href="index.html" {...{...props, className: `af-class-navigation-link w-nav-link w--current ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>Home</React.Fragment>}</a>)}{map(proxies['about-nav-link'], props => <a href="about.html" {...{...props, className: `af-class-navigation-link w-nav-link ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>About</React.Fragment>}</a>)}{map(proxies['contact-nav-link'], props => <a href="contact.html" {...{...props, className: `af-class-navigation-link w-nav-link ${props.className || ''}`}}>{props.children ? props.children : <React.Fragment>Contact</React.Fragment>}</a>)}</nav>
+              {map(proxies['navbar-menu-button'], props => <div {...{...props, className: `af-class-menu-button w-nav-button ${props.className || ''}`}}>{createScope(props.children, proxies => <React.Fragment>
+                {map(proxies['navbar-menu-icon'], props => <div {...{...props, className: `w-icon-nav-menu ${props.className || ''}`}}>{props.children}</div>)}
+              </React.Fragment>)}</div>)}
               {map(proxies['nav-overlay'], props => <div {...{...props, className: `af-class-navigationoverlay ${props.className || ''}`}}>{props.children}</div>)}
             </div>
           </div>
