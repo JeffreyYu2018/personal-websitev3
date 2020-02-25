3294e2163cbd09ea21ce2a1e0dcf213251e46f3b
JeffreyYu2018 <jeff.yu831@gmail.com>
2020-02-24 23:22:06 -0500

    Add title to web pages

--- src/controllers/AboutController.js
+++ src/controllers/AboutController.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import AboutView from '../views/AboutView'
+import { Helmet } from 'react-helmet'
 
 export default (props) => (
-  <AboutView>
-    <brand-nav-link href="/" />
-    <home-nav-link href="/" />
-    <about-nav-link href="/about" />
-    <contact-nav-link href="/contact" />
-    <get-in-touch-nav-link href="/contact" />
-  </AboutView>
+  <div>
+    <Helmet>
+      <title>About | Jeffrey Yu</title>
+    </Helmet>
+    <AboutView>
+      <brand-nav-link href="/" />
+      <home-nav-link href="/" />
+      <about-nav-link href="/about" />
+      <contact-nav-link href="/contact" />
+      <get-in-touch-nav-link href="/contact" />
+    </AboutView>
+  </div>
 )
