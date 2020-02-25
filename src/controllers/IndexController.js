12b172741c397258c20263cbc4ff95821f69d8bc
JeffreyYu2018 <jeff.yu831@gmail.com>
2020-02-25 14:34:51 -0500

    Experimentally remove appfairy sockets from nav bar

--- src/controllers/IndexController.js
+++ src/controllers/IndexController.js
@@ -2,7 +2,6 @@ import React from 'react';
 import IndexView from '../views/IndexView'
 import { withRouter } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
-import Sidebar from '../components/Sidebar'
 
 class IndexController extends React.Component {
   render() {
@@ -12,7 +11,7 @@ class IndexController extends React.Component {
           <title>Home | Jeffrey Yu</title>
         </Helmet>
         <IndexView>
-          <Sidebar />
+          <div>Test</div>
           <brand-nav-link href="/" />
           <home-nav-link href="/" />
           <about-nav-link href="/about" />
