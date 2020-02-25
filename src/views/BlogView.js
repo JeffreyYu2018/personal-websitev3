a9085b61beac16ef01e4a774059332aa7af58ef1
JeffreyYu2018 <jeff.yu831@gmail.com>
2020-02-24 19:04:33 -0500

    appfairy: Migrate

--- src/views/BlogView.js
+++ src/views/BlogView.js
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { createScope, map, transformProxies } from './helpers'
+import SidebarView from './SidebarView'
 
 const scripts = [
   fetch("https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.4.1.min.220afd743d.js").then(body => body.text()),
@@ -35,7 +36,7 @@ class BlogView extends React.Component {
     scripts.concat(Promise.resolve()).reduce((loaded, loading) => {
       return loaded.then((script) => {
         new Function(`
-          with (this) ${
+          with (this) {
             eval(arguments[0])
           }
         `).call(window, script)
@@ -82,11 +83,7 @@ class BlogView extends React.Component {
               <div className="w-container">
                 <div className="w-row">
                   <div className="w-hidden-small w-hidden-tiny w-col w-col-3">
-                    <div className="af-class-white-wrapper"><img src="images/LinkedIn-Photo-Square.jpg" srcSet="images/LinkedIn-Photo-Square-p-500.jpeg 500w, images/LinkedIn-Photo-Square-p-800.jpeg 800w, images/LinkedIn-Photo-Square-p-1080.jpeg 1080w, images/LinkedIn-Photo-Square-p-1600.jpeg 1600w, images/LinkedIn-Photo-Square-p-2000.jpeg 2000w, images/LinkedIn-Photo-Square-p-2600.jpeg 2600w, images/LinkedIn-Photo-Square-p-3200.jpeg 3200w, images/LinkedIn-Photo-Square.jpg 3456w" sizes="(max-width: 767px) 100vw, (max-width: 991px) 97.296875px, 126px" alt className="af-class-circle-profile" />
-                      <p className="af-class-site-description">Hi! My name is Jeffrey, and I wanted to welcome you to my site! I'm currently a student at Yale University studying computer science. I'm a software developer, tennis enthusiast, and travel blogger, and I'm excited to give you a look into my life. This is my temporary blog that I will be using until I&nbsp;develop my full personal website. Thank you for stopping by!</p>
-                      <div className="af-class-grey-rule" />
-                      <div className="af-class-social-link-group"><a href="https://www.facebook.com/jeffrey.yu.737" className="af-class-social-icon-link w-inline-block"><img src="images/social-03.svg" width={25} alt /></a><a href="https://www.instagram.com/jeffreyyu237/" className="af-class-social-icon-link w-inline-block"><img src="images/social-07.svg" width={25} alt /></a><a href="https://github.com/JeffreyYu2018" className="af-class-social-icon-link w-inline-block"><img src="images/github.svg" width={25} alt /></a><a href="https://www.linkedin.com/in/jeffrey-yu-/" className="af-class-social-icon-link w-inline-block"><img src="images/social-09.svg" width={25} alt /></a></div>
-                    </div>
+                    <SidebarView.Controller />
                   </div>
                   <div className="af-class-content-column w-col w-col-9">
                     <div className="af-class-post-wrapper">{map(proxies['post-image'], props => <img src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg" alt {...{...props, className: `af-class-post-image ${props.className || ''}`}}>{props.children}</img>)}
